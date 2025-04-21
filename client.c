240f90acd0d0da6c527c0590e516f4869aa8bb9c
Yulei <577990122@qq.com>
2025-04-21 13:32:44 +0200

    new server/client

--- client.c
+++ client.c
@@ -1,9 +1,9 @@
-#include <stdio.h>      // printf, scanf
-#include <stdlib.h>     // exit, malloc
-#include <string.h>     // memset, strcpy, strlen
-#include <unistd.h>     // read, write, close, sleep
-#include <sys/types.h>  // 基本系统类型
-#include <sys/socket.h> // socket, connect, write
-#include <sys/un.h>     // sockaddr_un 本地套接字结构
+#include <stdio.h>      
+#include <stdlib.h>     
+#include <string.h>     
+#include <unistd.h>     
+#include <sys/types.h>  
+#include <sys/socket.h> 
+#include <sys/un.h>     
 
-#define BUFFER_SIZE 1024   // 定义缓冲区大小为1024字节
+#define BUFFER_SIZE 1024   
