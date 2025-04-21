0dae8a09bd46ccd41b34664ac78dc0910aa39269
Yulei <577990122@qq.com>
2025-04-21 13:24:34 +0200

    new server/client

--- client.c
+++ client.c
@@ -1,72 +1,9 @@
-#include <stdio.h>        
-#include <stdlib.h>         
-#include <string.h>         
-#include <unistd.h>         
-#include <pthread.h>        
-#include <sys/socket.h>     
-#include <netinet/in.h>     
-#include <arpa/inet.h>      
+#include <stdio.h>      // printf, scanf
+#include <stdlib.h>     // exit, malloc
+#include <string.h>     // memset, strcpy, strlen
+#include <unistd.h>     // read, write, close, sleep
+#include <sys/types.h>  // 基本系统类型
+#include <sys/socket.h> // socket, connect, write
+#include <sys/un.h>     // sockaddr_un 本地套接字结构
 
-#define PORT 8080
-#define BUFFER_SIZE 1024
-
-
-void* traiter_client(void* arg) {
-    int sservice = *(int*)arg;  
-    char message[BUFFER_SIZE] = "Bienvenue sur le serveur !\n";  
-    write(sservice, message, strlen(message));  //vien de Prof. serveurUnix.c 
-    close(sservice);  
-    pthread_exit(NULL);  
-}
-
-int main() {
-    int secoute, sservice;
-    struct sockaddr_in saddr;  
-    struct sockaddr_in caddr;  
-    socklen_t caddrlen = sizeof(caddr);  
-    pthread_t thread_id;  
-    
-    secoute = socket(AF_INET, SOCK_STREAM, 0);
-    if (secoute == -1) {
-        perror("Erreur creation socket");
-        exit(1);
-    }
-
-
-    memset(&saddr, 0, sizeof(saddr));  
-    saddr.sin_family = AF_INET;        
-    saddr.sin_addr.s_addr = INADDR_ANY; 
-    saddr.sin_port = htons(PORT);      
-
-    if (bind(secoute, (struct sockaddr*)&saddr, sizeof(saddr)) < 0) {
-        perror("Erreur bind");
-        exit(1);
-    }
-
-
-    if (listen(secoute, 10) < 0) {
-        perror("Erreur listen");
-        exit(1);
-    }
-
-    printf("Serveur TCP en écoute sur le port %d...\n", PORT);
-
-    
-    while (1) {
-   
-        sservice = accept(secoute, (struct sockaddr*)&caddr, &caddrlen);
-        if (sservice < 0) {
-            perror("Erreur accept");
-            continue;
-        }
-
-        printf("Connexion de %s:%d\n", inet_ntoa(caddr.sin_addr), ntohs(caddr.sin_port));
-
-
-        pthread_create(&thread_id, NULL, traiter_client, (void*)&sservice);
-        pthread_detach(thread_id);  
-    }
-
-    close(secoute);  
-    return 0;
-}
+#define BUFFER_SIZE 1024   // 定义缓冲区大小为1024字节
