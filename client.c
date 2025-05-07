62c0e2c61ad273d448ff6623357a1d843590cc7b
kilox-afk <127036340+kilox-afk@users.noreply.github.com>
2025-05-07 19:20:01 +0000

    Add files via upload
    Comments

--- client.c
+++ client.c
@@ -15,17 +15,20 @@
 #define SERVER_IP "127.0.0.1"
 #define SERVER_PORT 8080
 
+// Variables globales pour la gestion du mode silencieux
 int sclient;
 volatile sig_atomic_t mode_silencieux = 0;
 char buffer_attente[100][BUFFER_SIZE];
 int nb_messages_attente = 0;
 pthread_mutex_t mutex_silence = PTHREAD_MUTEX_INITIALIZER;
 
+// Active le mode silencieux quand l'utilisateur appuie sur Ctrl+C
 void gestion_ctrl_c(int sig) {
     mode_silencieux = 1;
     printf("\n[SILENCIEUX] Mode silencieux activé. Tapez votre message :\n");
 }
 
+// Fonction du thread d'envoi de messages
 void* envoyer(void* arg) {
     char message[BUFFER_SIZE] = {0};
 
@@ -40,9 +43,10 @@ void* envoyer(void* arg) {
         write(sclient, message, strlen(message) + 1);
 
         if (message[0] == 'f') {
-            break;
+            break;  // Fin de communication
         }
 
+        // Si en mode silencieux, on affiche les messages mis en attente
         pthread_mutex_lock(&mutex_silence);
         if (mode_silencieux) {
             mode_silencieux = 0;
@@ -58,6 +62,7 @@ void* envoyer(void* arg) {
     pthread_exit(NULL);
 }
 
+// Fonction du thread de réception de messages
 void* recevoir(void* arg) {
     char reponse[BUFFER_SIZE] = {0};
 
@@ -65,10 +70,11 @@ void* recevoir(void* arg) {
         memset(reponse, '\0', BUFFER_SIZE);
         int n = read(sclient, reponse, BUFFER_SIZE);
 
-        if (n <= 0) break;
+        if (n <= 0) break;  // Fin de connexion
 
         pthread_mutex_lock(&mutex_silence);
         if (mode_silencieux) {
+            // Stocke les messages en attente si en mode silencieux
             if (nb_messages_attente < 100) {
                 strncpy(buffer_attente[nb_messages_attente++], reponse, BUFFER_SIZE);
             }
@@ -81,20 +87,24 @@ void* recevoir(void* arg) {
     pthread_exit(NULL);
 }
 
+// Fonction principale : connexion au serveur et gestion des threads
 int main(int argc, char *argv[]) {
     char message[BUFFER_SIZE] = {0};
     char reponse[BUFFER_SIZE] = {0};
     struct sockaddr_in saddr = {0};
     pthread_t t1, t2;
 
-    signal(SIGINT, gestion_ctrl_c);
+    signal(SIGINT, gestion_ctrl_c);  // Associe Ctrl+C au mode silencieux
 
+    // Initialisation de l'adresse du serveur
     saddr.sin_family = AF_INET;
     saddr.sin_port = htons(SERVER_PORT);
     saddr.sin_addr.s_addr = inet_addr(SERVER_IP);
 
+    // Création du socket client
     sclient = socket(AF_INET, SOCK_STREAM, 0);
 
+    // Tentative de connexion avec réessais
     int essais = 0;
     while (connect(sclient, (struct sockaddr*)&saddr, sizeof(saddr)) == -1 && essais < 5) {
         printf("Tentative %d échouée\n", essais + 1);
@@ -106,13 +116,15 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
+    // Saisie du nom de l'utilisateur
     char nom[NOM_SIZE];
     printf("Entrez votre nom : ");
     scanf("%s", nom);
-    getchar();
+    getchar(); // consomme le retour à la ligne restant dans le buffer
 
     write(sclient, nom, strlen(nom) + 1);
 
+    // Création des threads d'envoi et de réception
     pthread_create(&t1, NULL, envoyer, NULL);
     pthread_create(&t2, NULL, recevoir, NULL);
 
--- server.c
+++ server.c
@@ -15,12 +15,14 @@
 #define MAX_MESSAGES 100
 #define MSG_LENGTH 1024
 
+// Structure pour représenter un client
 typedef struct {
     int socket;
     char nom[NOM_SIZE];
     int id;
 } Client;
 
+// Variables globales
 Client clients[MAX_CLIENTS];
 int nb_clients = 0;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
@@ -28,17 +30,19 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 char historique_messages[MAX_CLIENTS][MAX_MESSAGES][MSG_LENGTH];
 int compteur_messages[MAX_CLIENTS] = {0};
 
+// Fonction exécutée dans un thread pour chaque client connecté
 void* traiter_client(void* arg) {
     Client client = *(Client*)arg;
     char message[BUFFER_SIZE] = {0};
     char annonce[FULL_SIZE] = {0};
 
+    // Lecture du nom du client
     read(client.socket, client.nom, NOM_SIZE);
 
     printf("[Serveur] Client %d (%s) a rejoint le chat.\n", client.id, client.nom);
-
     snprintf(annonce, sizeof(annonce), "[Serveur] Client %d (%s) a rejoint le chat.", client.id, client.nom);
 
+    // Diffusion de l'annonce aux autres clients
     pthread_mutex_lock(&mutex);
     for (int i = 0; i < nb_clients; i++) {
         if (clients[i].socket != client.socket) {
@@ -46,113 +50,3 @@ void* traiter_client(void* arg) {
         }
     }
     pthread_mutex_unlock(&mutex);
-
-    while (1) {
-        memset(message, '\0', BUFFER_SIZE);
-        int n = read(client.socket, message, BUFFER_SIZE);
-
-        if (n <= 0 || message[0] == 'f') break;
-
-        // Gestion historique messages
-        if (compteur_messages[client.id - 1] < MAX_MESSAGES) {
-            strncpy(historique_messages[client.id - 1][compteur_messages[client.id - 1]], message, MSG_LENGTH);
-            compteur_messages[client.id - 1]++;
-        }
-
-        // Afficher l'historique si le client le demande
-        if (strcmp(message, "historique") == 0) {
-            for (int i = 0; i < compteur_messages[client.id - 1]; i++) {
-                write(client.socket, historique_messages[client.id - 1][i], strlen(historique_messages[client.id - 1][i]) + 1);
-            }
-            continue;
-        }
-
-        printf("[Serveur] Message reçu de %s : %s\n", client.nom, message);
-
-        char full[FULL_SIZE];
-        snprintf(full, sizeof(full), "[Client %d - %s] %s", client.id, client.nom, message);
-
-        pthread_mutex_lock(&mutex);
-        for (int i = 0; i < nb_clients; i++) {
-            if (clients[i].socket != client.socket) {
-                write(clients[i].socket, full, strlen(full) + 1);
-            }
-        }
-        pthread_mutex_unlock(&mutex);
-    }
-
-    printf("[Serveur] Client %d (%s) a quitté le chat.\n", client.id, client.nom);
-    snprintf(annonce, sizeof(annonce), "[Serveur] Client %d (%s) a quitté le chat.", client.id, client.nom);
-
-    pthread_mutex_lock(&mutex);
-    for (int i = 0; i < nb_clients; i++) {
-        if (clients[i].socket == client.socket) {
-            clients[i] = clients[nb_clients - 1];
-            nb_clients--;
-            break;
-        }
-    }
-    for (int i = 0; i < nb_clients; i++) {
-        write(clients[i].socket, annonce, strlen(annonce) + 1);
-    }
-    pthread_mutex_unlock(&mutex);
-
-    close(client.socket);
-    pthread_exit(NULL);
-}
-
-int main() {
-    int secoute, sservice;
-    struct sockaddr_in saddr = {0};
-    struct sockaddr_in caddr = {0};
-    socklen_t caddrlen = sizeof(caddr);
-    pthread_t thread_id;
-
-    secoute = socket(AF_INET, SOCK_STREAM, 0);
-    if (secoute == -1) {
-        perror("Erreur création socket");
-        exit(1);
-    }
-
-    saddr.sin_family = AF_INET;
-    saddr.sin_addr.s_addr = INADDR_ANY;
-    saddr.sin_port = htons(PORT);
-
-    if (bind(secoute, (struct sockaddr*)&saddr, sizeof(saddr)) < 0) {
-        perror("Erreur bind");
-        exit(1);
-    }
-
-    listen(secoute, 10);
-    printf("Serveur en écoute sur le port %d...\n", PORT);
-
-    while (1) {
-        sservice = accept(secoute, (struct sockaddr*)&caddr, &caddrlen);
-        if (sservice < 0) {
-            perror("Erreur accept");
-            continue;
-        }
-
-        printf("Connexion de %s:%d\n", inet_ntoa(caddr.sin_addr), ntohs(caddr.sin_port));
-
-        Client new_client;
-        new_client.socket = sservice;
-        new_client.id = nb_clients + 1;
-
-        pthread_mutex_lock(&mutex);
-        if (nb_clients < MAX_CLIENTS) {
-            clients[nb_clients++] = new_client;
-        } else {
-            close(new_client.socket);
-            pthread_mutex_unlock(&mutex);
-            continue;
-        }
-        pthread_mutex_unlock(&mutex);
-
-        pthread_create(&thread_id, NULL, traiter_client, (void*)&clients[nb_clients - 1]);
-        pthread_detach(thread_id);
-    }
-
-    close(secoute);
-    return 0;
-}
